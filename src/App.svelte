edc9968207e547ee994eb29daefb4553c60ba34f
jckr <jckr.github@gmail.com>
2022-09-20 16:40:19 -0700

    ask for permission then listen to device orientation

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,49 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import {onMount} from 'svelte';
+
+  let orientation = {
+    alpha: 0,
+    beta: 0,
+    gamma: 0,
+  };
+  onMount(async () => {
+    if (typeof DeviceOrientationEvent?.requestPermission === 'function') {
+      DeviceOrientationEvent?.requestPermission()
+        .then((response) => {
+          if (response == 'granted') {
+            window.addEventListener('deviceorientation', (e) => {
+              orientation = {
+                alpha: e.alpha,
+                beta: e.beta,
+                gamma: e.gamma,
+              };
+            });
+          }
+        })
+        .catch(console.error);
+    } else {
+      Promise.resolve(
+        window.addEventListener('deviceorientation', (e) => {
+          orientation = {
+            alpha: e.alpha,
+            beta: e.beta,
+            gamma: e.gamma,
+          };
+        })
+      );
+    }
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Device Orientation</h1>
+  {JSON.stringify(orientation)}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
   }
 </style>
