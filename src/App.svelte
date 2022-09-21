826095589f870b44ff7aba8c18ac00de5b58e6f1
jckr <jckr.github@gmail.com>
2022-09-20 17:01:51 -0700

    add location tracking

--- src/App.svelte
+++ src/App.svelte
@@ -6,32 +6,51 @@
     beta: 0,
     gamma: 0,
   };
-  onMount(async () => {
-    if (typeof DeviceOrientationEvent?.requestPermission === 'function') {
-      DeviceOrientationEvent?.requestPermission()
-        .then((response) => {
-          permission = response;
-          if (response == 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
+  $: location = {
+    latitude: 0,
+    longitude: 0,
+    altitude: 0,
+    accuracy: 0,
+    altitudeAccuracy: 0,
+    heading: 0,
+    speed: 0,
+  };
+
+  function createListeners() {
+    window.addEventListener('deviceorientation', (e) => {
               orientation = {
                 alpha: e.alpha,
                 beta: e.beta,
                 gamma: e.gamma,
               };
             });
+    navigator?.geolocation?.watchPosition((position) => {
+              location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                altitude: position.coords.altitude,
+                accuracy: position.coords.accuracy,
+                altitudeAccuracy: position.coords.altitudeAccuracy,
+                heading: position.coords.heading,
+                speed: position.coords.speed,
+              };
+            });
+  }
+
+  onMount(async () => {
+    if (typeof DeviceOrientationEvent?.requestPermission === 'function') {
+      DeviceOrientationEvent?.requestPermission()
+        .then((response) => {
+          permission = response;
+          if (response == 'granted') {
+            createListeners();
           }
         })
         .catch(console.error);
     } else {
       permission = 'no requestPermission function';
       Promise.resolve(
-        window.addEventListener('deviceorientation', (e) => {
-          orientation = {
-            alpha: e.alpha,
-            beta: e.beta,
-            gamma: e.gamma,
-          };
-        })
+        createListeners();
       );
     }
   });
@@ -39,7 +58,9 @@
 
 <main>
   <h1>Device Orientation</h1>
-  <h3>{permission}</h3>
-  {JSON.stringify(orientation)}
+  <h3>{`Permission: ${permission}`}</h3>
+  {JSON.stringify(orientation, null, 2)}
+  <h3>Geolocation</h3>
+  {JSON.stringify(location, null, 2)}
 </main>
 
