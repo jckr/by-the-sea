1ed76a6727f6a22c1541dfd5406b30d113ffd362
jckr <jckr.github@gmail.com>
2022-09-26 14:24:33 -0700

    simulate deviceorientation with mouse

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import {onMount} from 'svelte';
-  $: permission = ''; 
+  $: permission = '';
   $: orientation = {
     alpha: 0,
     beta: 0,
@@ -15,26 +15,59 @@
     heading: 0,
     speed: 0,
   };
-
+  let x = 0, y = 0, dragging = false;
+  let shift = false;
+  function handleDragStart(e) {
+    x = e.clientX;
+    y = e.clientY;
+    dragging = true;
+    shift = e.shiftKey;
+  }
+  function handleDragEnd(e) {
+    x = e.clientX;
+    y = e.clientY;
+    dragging = false;
+  }
+  function handleDrag(e) {
+    if (dragging === false) return;
+    const dx = e.clientX - x;
+    const dy = e.clientY - y;
+    x += dx;
+    y += dy;
+    orientation.alpha += dx;
+    if (shift) {
+      orientation.gamma += dy;
+    } else {
+      orientation.beta += dy;
+    }
+    orientation = sanitize(orientation);
+  }
+  function sanitize({alpha,beta, gamma}) {
+    alpha = (alpha + 360) % 360;
+    beta = Math.max(-90, Math.min(90, beta));
+    gamma = Math.max(-90, Math.min(90, gamma));
+    return {alpha,beta, gamma};
+  }
   function createListeners() {
     window.addEventListener('deviceorientation', (e) => {
-              orientation = {
-                alpha: e.alpha,
-                beta: e.beta,
-                gamma: e.gamma,
-              };
-            });
+      orientation = {
+        alpha: e.alpha,
+        beta: e.beta,
+        gamma: e.gamma,
+      };
+    });
+
     navigator?.geolocation?.watchPosition((position) => {
-              location = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                altitude: position.coords.altitude,
-                accuracy: position.coords.accuracy,
-                altitudeAccuracy: position.coords.altitudeAccuracy,
-                heading: position.coords.heading,
-                speed: position.coords.speed,
-              };
-            });
+      location = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        altitude: position.coords.altitude,
+        accuracy: position.coords.accuracy,
+        altitudeAccuracy: position.coords.altitudeAccuracy,
+        heading: position.coords.heading,
+        speed: position.coords.speed,
+      };
+    });
   }
 
   onMount(async () => {
@@ -49,18 +82,18 @@
         .catch(console.error);
     } else {
       permission = 'no requestPermission function';
-      Promise.resolve(
-        createListeners()
-      );
+      Promise.resolve(createListeners());
     }
   });
 </script>
 
-<main>
-  <h1>Device Orientation</h1>
+<main  
+  on:mousedown={handleDragStart}
+  on:mousemove={handleDrag}
+  on:mouseup={handleDragEnd}>
+  <h1 >Device Orientation</h1>
   <h3>{`Permission: ${permission}`}</h3>
   {JSON.stringify(orientation, null, 2)}
   <h3>Geolocation</h3>
   {JSON.stringify(location, null, 2)}
 </main>
-
